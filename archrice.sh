f79768fc40609003b00bfcf4a6ee26191df9c09a
KostasEreksonas <k.ereksonas@gmail.com>
2022-08-17 22:44:04 +0300

    Display suckless utility name in dialog window when installing the utility

--- archrice.sh
+++ archrice.sh
@@ -522,7 +522,7 @@ function installWM () {
 	done
 
 	for (( j=0; j<$len; j++ )); do
-		dialog --title "Installing Window Manager" --infobox "Installing ${suckless_utilities[$i]}" 0 0
+		dialog --title "Installing Window Manager" --infobox "Installing ${suckless_utilities[$j]}" 0 0
 		cd ${suckless_utilities[$j]}/
 		make 2>>$logfile 1>&2
 		make clean install 2>>$logfile 1>&2
