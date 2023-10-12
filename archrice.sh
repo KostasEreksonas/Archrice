52a5317e8a1c2f283ef79e9b9672685b9f96ffa4
KostasEreksonas <k.ereksonas@gmail.com>
2023-10-12 17:06:20 +0300

    Added function for creating an user

--- archrice.sh
+++ archrice.sh
@@ -34,6 +34,66 @@ function configurePacman() {
 	cd /etc/ && sed -i '33s/#//' pacman.conf; sed -i '37s/#//' pacman.conf; sed -i '93s/#//' pacman.conf; sed -i '94s/#//' pacman.conf
 }
 
+# Create and configure a new system user
+function createUser() {
+	# Pick an username (cannot be empty)
+	while [ -z $name ]; do
+		name=$(dialog --stdout --title "User Creation" --inputbox "User Name:" 0 0)
+		if [ -z $name ]; then
+			dialog --title "User Creation" --msgbox "Username cannot be empty. Try again" 0 0
+		else
+			useradd -m $name && homedir=/home/$name && username=$name
+		fi
+	done
+
+	# Choose groups to add the user to
+	groups=$(dialog --stdout --title "User Creation" \
+			--checklist "Choose groups where the user $username should be added to:" 0 0 0 \
+			wheel wheel on \
+			adm adm off \
+			ftp ftp off \
+			games games off \
+			http http off \
+			log log off \
+			rfkill rfkill off \
+			sys sys off \
+			systemd-journal systemd-journal off \
+			uucp uucp off \
+			libvirt libvirt on \
+			dbus dbus off \
+			kmem kmem off \
+			locate locate off \
+			lp lp off \
+			mail mail off \
+			nobody nobody off \
+			proc proc off \
+			root root off \
+			smmsp smmsp off \
+			tty tty off \
+			utmp utmp off \
+			audio audio on \
+			disk disk off \
+			floppy floppy off \
+			input input off \
+			kvm kvm off \
+			video video on \
+			optical optical on \
+			storage storage on | sed 's/ /,/g') && usermod -aG $groups $name
+
+	# Create a password for new user
+	while [ $password != $passcheck ]; do
+		password=$(dialog --stdout --title "User Creation" --passwordbox "Enter password for user $name:" 0 0)
+		passcheck=$(dialog --stdout --title "User Creation" --passwordbox "Re-enter password for user $name:" 0 0)
+		if [ $password != $passcheck ]; then
+			unset $passcheck && userError $name || break
+		else
+			unset $passcheck && echo "$name:$password" | chpasswd
+		fi
+	done
+
+	return $?
+}
+
 #  -------------
 # | Main Script |
 #  -------------
@@ -41,5 +101,6 @@ function configurePacman() {
 while [ $? == 0 ]; do
 	welcomeMsg
 	configurePacman
+	createUser
 	exitMsg
 done
